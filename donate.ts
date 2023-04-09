eabfccc172f4bdee80406d4169cb6cf2a11f9a33
Cheru Berhanu <mail@cheru.dev>
2023-04-09 17:46:50 -0400

    Delete donate.ts

--- donate.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.redirect('https://bank.hackclub.com/donations/start/green-mountain-robotics')
-}
