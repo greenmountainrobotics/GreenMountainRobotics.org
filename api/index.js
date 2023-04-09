7c2027e7bccf18e26ba624cd20c3da8980f78a6f
Cheru Berhanu <mail@cheru.dev>
2023-04-09 17:49:32 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  return res.redirect("https://bank.hackclub.com/donations/start/green-mountain-robotics")
+  res.redirect("https://bank.hackclub.com/donations/start/green-mountain-robotics")
 });
 
 module.exports = app;
