720266196f1ac4c11ab66abde0dfe2c19e77a11e
Cheru Berhanu <mail@cheru.dev>
2023-04-09 17:52:35 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const app = require('express')();
-const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
   res.redirect("https://bank.hackclub.com/donations/start/green-mountain-robotics")
