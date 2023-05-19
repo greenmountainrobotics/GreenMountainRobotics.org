01b3c464ff626161e032dfb069c8c8d77e95520b
Zexn1x <sthilairelucas61@gmail.com>
2023-05-19 16:02:00 -0400

    Better looking

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
-
-
-function OnSubmit() {
-    document.getElementById("FormResponse").innerHTML = "Thank you for submiting an application to join our team! We will try to get back to you in the next week through email to give you more information.";
+setTimeout(()=> {
+    
+function OnSubmit() 
+    document.getElementById("myForm").reset();
 }
+,3000);
