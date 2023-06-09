0a07c4f043f34981291386cd05c7eaab5a8b0e56
Zexn1x <sthilairelucas61@gmail.com>
2023-06-08 21:16:16 -0400

    New Website

--- public/new-ideas/script.js
+++ public/new-ideas/script.js
@@ -1,3 +1,19 @@
+// Form Clears
 function clearForm() {
     document.getElementById("myForm").reset();
 }
+
+// Calendar Date
+const d = new Date();
+let currentDay = d.getDate();
+let currentMonth = d.getMonth();
+let currentYear = d.getFullYear();
+
+// Date at top of calendar
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const month = months[currentMonth];
+document.getElementById("table-month").innerHTML = month + " " + currentYear;
+
+const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
